c3378cdd252f56f9ed06db04b0714fe3e26ef5d1
Yassir-cpu <84718803+Yassir-cpu@users.noreply.github.com>
2023-02-15 01:15:34 +0100

    Update Class1.java
    ok

--- Class1.java
+++ Class1.java
@@ -1,6 +1,6 @@
 public class Class1 {
   public static void main(String[] args) {
-    System.out.println("Hello World");
-    System.out.println("Des nouvelles modifications sont apportées");
+    System.out.println("Hello World 2");
+    
   }
 }
